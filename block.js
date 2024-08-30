a6fbad100c01993e3930f36515d28e014cbc855f
Sven Kanter <122672556+cjanta@users.noreply.github.com>
2024-08-30 09:22:31 +0000

    ++

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+
+const SHA256 = require("crypto-js/sha256");
+
 class Block{
 
     constructor(timestamp, lastHash, hash, data){
@@ -22,9 +25,13 @@ class Block{
     static mineBlock(lastBlock, data){
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-        const hash = " to Do hash";
+        const hash =  Block.hash(timestamp, lastHash, data);
 
         return new this(timestamp, lastHash, hash , data);
+    }
+
+    static hash(timestamp, lastHash, data){
+        return SHA256(`${timestamp}${lastHash}${data}`).toString();
     }
     
 }
--- dev-test.js
+++ dev-test.js
@@ -3,6 +3,9 @@ console.log("Example implementation of the worlds biggest casino.");
 const Block = require("./block");
 
 const block_chain = [];
+block_chain.push(Block.genesis());
 
-const test_block = Block.mineBlock(Block.genesis(), "Testdaten!");
-console.log(test_block.toString());
+const test_block = Block.mineBlock(block_chain[0], "Testdaten!");
+block_chain.push(test_block);
+
+block_chain.forEach(block => console.log(block));
